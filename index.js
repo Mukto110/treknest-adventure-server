82ff39ad3a55cc272ab15f78ffa1d10532e64538
Meraj Hossain Mukto <merajhossainmukto@Merajs-MacBook-Air.local>
2025-03-01 10:34:14 +0600

    db connection, dotenv, port, cors setup done

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+
+dotenv.config();
 
 const app = express();
 const port = 3000;
