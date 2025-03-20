f806f785b12d15ef3da514283d0339af4839c0ff
mukto110 <merajhossainmukto@gmail.com>
2025-03-21 02:57:38 +0600

    insert a new tourist spot and get all tourist spot api has been done

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 require("dotenv").config();
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
-const app = express();
 const port = process.env.PORT || 5000;
+const app = express();
+
+// Middleware
+const corsOptions = {
+  origin: ["http://localhost:5173", "http://localhost:5174"],
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+app.use(express.json());
 
 const uri = process.env.MONGO_URI;
-
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -17,18 +26,45 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
 });
 
-async function connectDB() {
+async function run() {
   try {
     await client.connect();
     console.log("✅ Successfully connected to MongoDB!");
+
+    const touristSpotCollection = client
+      .db("treknest")
+      .collection("tourist_spots");
+
+    // Get all tourist spots
+    app.get("/api/tourist-spots", async (req, res) => {
+      try {
+        const touristSpots = await touristSpotCollection.find().toArray();
+        res.status(200).json(touristSpots);
+      } catch (error) {
+        res.status(500).json({ message: "Failed to fetch tourist spots" });
+      }
+    });
+
+    // Get a single tourist spot by ID
+
+    // Insert a new tourist spot
+    app.post("/api/tourist-spots", async (req, res) => {
+      const spotData = req.body;
+      const result = await touristSpotCollection.insertOne(spotData);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. Successfully connected to MongoDB!");
   } catch (err) {
     console.error("❌ MongoDB Connection Failed:", err);
   }
 }
-connectDB();
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Hello World! MongoDB is connected.");
+  res.send("Hello from TrekNest server");
 });
 
 app.listen(port, () => {
